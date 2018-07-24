74d66f5ea28f42918d90912062321b50d74fe0d8
Jose Otmaro Gonzalez Esquivel <otmaro@outlook.com>
2018-07-24 16:23:55 -0600

    Exposing PurchaseModule on app.mudel.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
+import { PurchaseModule } from './purchase/purchase.module';
 const { DB_NAME: dbname, DB_USER: dbuser, DB_HOST: dbhost, DB_PORT: dbport } = process.env;
 const dburi = `mongodb://${dbuser}@${dbhost}:${dbport}/${dbname}`;
 
 @Module({
-    imports: [MongooseModule.forRoot(dburi)],
+    imports: [MongooseModule.forRoot(dburi), PurchaseModule],
     controllers: [AppController],
     providers: [AppService],
 })
