7c2d3d396b2ef344ed3b7d7bd2189a7a16264f78
Jose Otmaro Gonzalez Esquivel <otmaro@outlook.com>
2018-07-24 17:46:56 -0600

    Added Constructure

--- src/purchase/dto/create-purchase.dto.ts
+++ src/purchase/dto/create-purchase.dto.ts
@@ -2,5 +2,12 @@ export class CreatePurchaseDto {
     readonly userId: String;
     readonly productId: String;
     readonly created: Date;
-    readonly modified: Date
+    readonly modified: Date;
+
+    constructor(userId: string, productId: string) {
+        this.userId = userId;
+        this.productId = productId;
+        this.created = new Date();
+        this.modified = new Date();
+    }
 }
