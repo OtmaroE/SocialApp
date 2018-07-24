0510ef5b9db087e633855ee09cec672e3a1936ff
Jose Otmaro Gonzalez Esquivel <otmaro@outlook.com>
2018-07-24 17:47:29 -0600

    Fixe typo

--- src/purchase/purchase.module.ts
+++ src/purchase/purchase.module.ts
@@ -2,10 +2,10 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { PurchaseController } from './purchase.controller';
 import { PurchaseService } from './purchase.service';
-import { PurchaseSchema } from './Schemas/purchase.schema';
+import { PurchaseSchema } from './schemas/purchase.schema';
 
 @Module({
-    imports: [MongooseModule.forFeature([{name: 'Purchase', schema: PurchaseSchema}])],
+    imports: [MongooseModule.forFeature([{ name: 'Purchase', schema: PurchaseSchema }])],
     controllers: [PurchaseController],
     providers: [PurchaseService],
 })
