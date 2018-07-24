f69aaada46135edf2fbd1705172fbf257d722e14
Pablo Berganza <pberganza10@gmail.com>
2018-07-24 12:35:14 -0600

    ADDED: Mongoose Model Product

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ProductsController } from './products.controller';
 import { ProductsService } from './products.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ProductSchema } from './schemas/product.schema';
 
 @Module({
-  controllers: [ProductsController],
-  providers: [ProductsService]
+    imports: [MongooseModule.forFeature([{ name: 'Product', schema: ProductSchema }])],
+    controllers: [ProductsController],
+    providers: [ProductsService],
 })
-export class ProductsModule {}
+export class ProductsModule { }
