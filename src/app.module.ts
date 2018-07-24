62b3a615713160bf13947c643512fae5f99b22a5
Pablo Berganza <pberganza10@gmail.com>
2018-07-24 12:07:30 -0600

    ADDED: Connection string

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+const { DB_NAME: dbname, DB_USER: dbuser, DB_HOST: dbhost, DB_PORT: dbport } = process.env;
+const dburi = `mongodb://${dbuser}@${dbhost}:${dbport}/${dbname}`;
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [],
+    controllers: [AppController],
+    providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
