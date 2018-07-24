7a0711d058be97b32304f29f882124e68be0f6cd
Kevin <kaleman960@gmail.com>
2018-07-24 15:18:32 -0600

    User schema added

--- src/users/schemas/users.schema.ts
+++ src/users/schemas/users.schema.ts
@@ -1,1 +1,23 @@
 import * as mongoose from 'mongoose';
+import * as bcrypt from 'bcrypt';
+
+export const UserSchema = new mongoose.Schema({
+  username: String,
+  password: String,
+  creditLimit: Number,
+  created: {
+    type: Date,
+    default: Date.now,
+  },
+  modified: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+UserSchema.statics.hashPassword = (pass) => {
+  return bcrypt.hashSync(pass, bcrypt.genSaltSync(10));
+};
+UserSchema.methods.comparePass = function(pass) {
+  return bcrypt.compareSync(pass, this.password);
+};
