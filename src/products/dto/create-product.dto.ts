ed67bdabf02ad4f35525b0b7c908ae626236cb84
Pablo Berganza <pberganza10@gmail.com>
2018-07-24 17:14:06 -0600

    ADDED: validation

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,5 +1,12 @@
+import { IsString, IsNumber } from 'class-validator';
+
 export class CreateProductDto {
+    @IsString()
     readonly name: string;
+
+    @IsNumber()
     readonly price: number;
+
+    @IsString()
     readonly brand: string;
 }
