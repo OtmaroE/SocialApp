1bfa9c50540ec9916531df07ce1ad8157d7d98cf
Pablo Berganza <pberganza10@gmail.com>
2018-07-24 12:11:03 -0600

    ADDED: Mongoose Connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
 const { DB_NAME: dbname, DB_USER: dbuser, DB_HOST: dbhost, DB_PORT: dbport } = process.env;
 const dburi = `mongodb://${dbuser}@${dbhost}:${dbport}/${dbname}`;
 
 @Module({
-    imports: [],
+    imports: [MongooseModule.forRoot(dburi)],
     controllers: [AppController],
     providers: [AppService],
 })
