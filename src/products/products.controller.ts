3de38a94c870f682d65d54cb408d1abd2976767f
Pablo Berganza <pberganza10@gmail.com>
2018-07-24 14:02:35 -0600

    ADDED: findAll and Create

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,4 +1,28 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Delete, Param } from '@nestjs/common';
+import { ProductsService } from './products.service';
+import { CreateProductDto } from './dto/create-product.dto';
 
 @Controller('products')
-export class ProductsController {}
+export class ProductsController {
+    constructor(private readonly productsService: ProductsService) { }
+
+    @Get()
+    findAll() {
+        this.productsService.findAll();
+    }
+
+    @Get(':id')
+    findOne(@Param('id') id: string) {
+
+    }
+
+    @Post()
+    create(createProductDto: CreateProductDto) {
+        this.productsService.create(createProductDto);
+    }
+
+    @Delete(':id')
+    deleteById(@Param('id') id: string) {
+
+    }
+}
--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Product } from './interfaces/product.interface';
+import { Model } from 'mongoose';
+import { CreateProductDto } from './dto/create-product.dto';
 
 @Injectable()
-export class ProductsService {}
+export class ProductsService {
+    constructor(@InjectModel('Product') private readonly productModel: Model<Product>) { }
+
+    async findAll(): Promise<Product[]> {
+        return await this.productModel.find().exec();
+    }
+
+    async create(createProductDto: CreateProductDto): Promise<Product> {
+        return await this.productModel.create(createProductDto);
+    }
+}
