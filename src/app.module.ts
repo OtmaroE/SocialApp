dbaddd8bfa74482f0664d349928e4220e2885887
Pablo Berganza <pberganza10@gmail.com>
2018-07-24 14:36:00 -0600

    FIXED: Connection string

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,7 +4,7 @@ import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ProductsModule } from './products/products.module';
 const { DB_NAME: dbname, DB_USER: dbuser, DB_HOST: dbhost, DB_PORT: dbport } = process.env;
-const dburi = `mongodb://${dbuser}@${dbhost}:${dbport}/${dbname}`;
+const dburi = `mongodb://${dbhost}:${dbport}/${dbname}`;
 
 @Module({
     imports: [MongooseModule.forRoot(dburi), ProductsModule],
