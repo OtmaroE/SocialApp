f0fbd67ee282054828ed4df8ad597a62e084f48c
Kevin <kaleman960@gmail.com>
2018-07-25 12:04:59 -0600

    Something

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,15 +2,14 @@ import { Module, NestModule, MiddlewareConsumer, RequestMethod } from '@nestjs/c
 import { AppController } from 'app.controller';
 import { AppService } from 'app.service';
 import { MongooseModule } from '@nestjs/mongoose';
-import { ProductsModule } from './products/products.module';
-import { PurchaseModule } from './purchase/purchase.module';
-import { UsersModule } from './users/users.module';
-import { attachUserToReq } from './middleware/user.middleware';
-const { DB_NAME: dbname, DB_USER: dbuser, DB_HOST: dbhost, DB_PORT: dbport } = process.env;
-const dburi = `mongodb://${dbhost}:${dbport}/${dbname}`;
+import { ProductsModule } from 'products/products.module';
+import { PurchaseModule } from 'purchase/purchase.module';
+import { UsersModule } from 'users/users.module';
+import { attachUserToReq } from 'middleware/user.middleware';
+const { MONGO_URI } = process.env;
 
 @Module({
-    imports: [MongooseModule.forRoot(dburi), ProductsModule, PurchaseModule, UsersModule],
+    imports: [MongooseModule.forRoot(MONGO_URI), ProductsModule, PurchaseModule, UsersModule],
     controllers: [AppController],
     providers: [AppService],
 })
