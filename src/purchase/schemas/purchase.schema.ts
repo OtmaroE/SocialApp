85c6837a708b2445834bfa456b98d0140f5a30de
KevLehman <kaleman960@gmail.com>
2018-07-25 13:59:50 -0600

    Format
    Added spaces to the object in created

--- src/purchase/schemas/purchase.schema.ts
+++ src/purchase/schemas/purchase.schema.ts
@@ -3,7 +3,10 @@ import * as mongoose from 'mongoose';
 export const PurchaseSchema = new mongoose.Schema({
     userId: String,
     productId: String,
-    created: { type: Date, default: Date.now },
+    created: { 
+        type: Date, 
+        default: Date.now 
+    },
     modified: Date,
 });
 
