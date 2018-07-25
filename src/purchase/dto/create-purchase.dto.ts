67bfdc40c9efbef44b3ca3f4b7078618e2f8e13b
Pablo Berganza <pberganza10@gmail.com>
2018-07-25 11:06:32 -0600

    FIXED: String->string

--- src/purchase/dto/create-purchase.dto.ts
+++ src/purchase/dto/create-purchase.dto.ts
@@ -1,6 +1,6 @@
 export class CreatePurchaseDto {
-    readonly userId: String;
-    readonly productId: String;
+    readonly userId: string;
+    readonly productId: string;
     readonly created: Date;
     readonly modified: Date;
 
