38d1d243be350d74474c7235cf0656d1c7d1aa30
Kevin <kaleman960@gmail.com>
2018-07-24 18:09:45 -0600

    Added UsersModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,22 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module, NestModule, MiddlewareConsumer, RequestMethod } from '@nestjs/common';
+import { AppController } from 'app.controller';
+import { AppService } from 'app.service';
 import { MongooseModule } from '@nestjs/mongoose';
-const { DB_NAME: dbname, DB_USER: dbuser, DB_HOST: dbhost, DB_PORT: dbport } = process.env;
-const dburi = `mongodb://${dbuser}@${dbhost}:${dbport}/${dbname}`;
+import { UsersModule } from './users/users.module';
+import { attachUserToReq } from './middleware/user.middleware';
+
+const { MONGO_URI } = process.env;
 
 @Module({
-    imports: [MongooseModule.forRoot(dburi)],
+    imports: [MongooseModule.forRoot(MONGO_URI, { useNewUrlParser: true }), UsersModule],
     controllers: [AppController],
     providers: [AppService],
 })
-export class AppModule { }
+
+export class AppModule implements NestModule {
+    configure(consumer: MiddlewareConsumer) {
+        consumer
+            .apply(attachUserToReq)
+            .forRoutes({ path: '*', method: RequestMethod.ALL });
+    }
+}
