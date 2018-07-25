0e7e4b7d1465dface475a30ca82a68c5ae9504b2
Pablo Berganza <pberganza10@gmail.com>
2018-07-24 18:00:42 -0600

    ADDED: PurchaseModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ProductsModule } from './products/products.module';
+import { PurchaseModule } from './purchase/purchase.module';
 const { DB_NAME: dbname, DB_USER: dbuser, DB_HOST: dbhost, DB_PORT: dbport } = process.env;
 const dburi = `mongodb://${dbhost}:${dbport}/${dbname}`;
 
 @Module({
-    imports: [MongooseModule.forRoot(dburi), ProductsModule],
+    imports: [MongooseModule.forRoot(dburi), ProductsModule, PurchaseModule],
     controllers: [AppController],
     providers: [AppService],
 })
