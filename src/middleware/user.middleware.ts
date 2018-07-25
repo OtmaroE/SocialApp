22a27d14df7067b948bd2ca1e881b8b693cd5cf6
Kevin <kaleman960@gmail.com>
2018-07-25 11:13:44 -0600

    Fixed datatype for valid binding

--- src/middleware/user.middleware.ts
+++ src/middleware/user.middleware.ts
@@ -1,9 +1,9 @@
-import { validateJWT } from '../users/helpers/jwt';
+import { validateJWT } from 'users/helpers/jwt';
 
 export function attachUserToReq(req, res, next) {
   req.user = {};
   if (!req.headers.authorization || req.headers.authorization === null) return next();
-  const valid = validateJWT(req.headers.authorization);
+  const valid: any = validateJWT(req.headers.authorization);
   if (valid.id) req.user = valid;
   next();
 }
