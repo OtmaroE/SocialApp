26ad57f56761e627c352108044ddfa208c2ec46d
Pablo Berganza <pberganza10@gmail.com>
2018-07-25 10:50:28 -0600

    ADDED: sets modified date

--- src/purchase/schemas/purchase.schema.ts
+++ src/purchase/schemas/purchase.schema.ts
@@ -4,5 +4,10 @@ export const PurchaseSchema = new mongoose.Schema({
     userId: String,
     productId: String,
     created: Date,
-    modified: Date
+    modified: Date,
+});
+
+PurchaseSchema.pre('save', function(next) {
+    this.modified = new Date();
+    next();
 });
