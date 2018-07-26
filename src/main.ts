ac4f046109508afcaec584aadaef3919f673017e
Jose Otmaro Gonzalez Esquivel <otmaro@outlook.com>
2018-07-26 17:29:54 -0600

    Swagger added to Project

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,20 @@
 import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from 'app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
+    const options = new DocumentBuilder()
+        .setTitle('Bistec API')
+        .setDescription('Owed payment record keeping for `el bistec`')
+        .setVersion('1.0')
+        .addBearerAuth('Json Web Token','header')
+        .setBasePath('api')
+        .build();
+    const document = SwaggerModule.createDocument(app, options);
+    SwaggerModule.setup('explorer', app, document);
+
     app.setGlobalPrefix('api');
     await app.listen(3000);
 }
