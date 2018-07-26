5ef8357e67ebf8ad5029d6c2e6234d64050c92bb
Jose Otmaro Gonzalez Esquivel <otmaro@outlook.com>
2018-07-26 11:25:44 -0600

    Import PaymentModule

--- src/purchase/purchase.module.ts
+++ src/purchase/purchase.module.ts
@@ -5,12 +5,14 @@ import { PurchaseService } from './purchase.service';
 import { PurchaseSchema } from './schemas/purchase.schema';
 import { ProductsModule } from '../products/products.module';
 import { UsersModule } from '../users/users.module';
+import { PaymentModule } from 'payment/payment.module';
 
 @Module({
     imports: [
         MongooseModule.forFeature([{ name: 'Purchase', schema: PurchaseSchema }]),
         forwardRef(() => ProductsModule),
-        UsersModule
+        UsersModule,
+        PaymentModule
     ],
     controllers: [PurchaseController],
     providers: [PurchaseService],
