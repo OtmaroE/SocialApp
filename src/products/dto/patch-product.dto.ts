51ed3a1999cf0d75d7065a24ac94691d2ba786c5
Jose Otmaro Gonzalez Esquivel <otmaro@outlook.com>
2018-07-26 17:31:13 -0600

    Product Endpoints are documented with Swagger

--- src/products/dto/patch-product.dto.ts
+++ src/products/dto/patch-product.dto.ts
@@ -1,15 +1,19 @@
 import { IsString, IsNumber, IsOptional } from 'class-validator';
+import { ApiModelProperty, ApiModelPropertyOptional } from '@nestjs/swagger';
 
 export class PatchProductDto {
     @IsOptional()
     @IsString()
+    @ApiModelPropertyOptional()
     readonly name?: string;
 
     @IsOptional()
     @IsNumber()
+    @ApiModelPropertyOptional()
     readonly price?: number;
 
     @IsOptional()
     @IsString()
+    @ApiModelPropertyOptional()
     readonly brand?: string;
 }
--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -7,9 +7,11 @@ import { PatchProductDto } from './dto/patch-product.dto';
 import { Product } from './interfaces/product.interface';
 import { Purchase } from '../purchase/interfaces/purchase.interface';
 import { Validator } from 'class-validator';
+import { ApiUseTags, ApiBearerAuth, ApiResponse, ApiConsumes } from '@nestjs/swagger';
 const validator = new Validator();
 
 @Controller('products')
+@ApiUseTags('Products')
 export class ProductsController {
     constructor(
         private readonly productsService: ProductsService,
@@ -17,29 +19,40 @@ export class ProductsController {
     ) { }
 
     @Get()
+    @ApiBearerAuth()
+    @ApiResponse( { status: 200, description: 'List of products was generated.' })
     findAll(): Promise<Product[]> {
         return this.productsService.findAll();
     }
 
     @Get(':id')
+    @ApiBearerAuth()
+    @ApiResponse({ status: 200, description: 'Product was found.' })
+    @ApiResponse({ status: 404, description: 'Product was NOT found.' })
     findOne(@Param('id') id: string) {
         if (!validator.isMongoId(id)) throw new BadRequestException();
         return this.productsService.findOne(id);
     }
 
     @Post()
+    @ApiBearerAuth()
+    @ApiResponse({ status: 201, description: 'Product was created.' })
     @UsePipes(new ValidationPipe({ transform: true }))
     create(@Body() createProductDto: CreateProductDto): Promise<Product> {
         return this.productsService.create(createProductDto);
     }
 
     @Post(':id/purchase')
+    @ApiBearerAuth()
+    @ApiResponse({ status: 201, description: 'Purchase was registered.' })
     createPurchase(@Param('id') id: string, @Req() req): Promise<Purchase> {
         const createPurchaseDto = new CreatePurchaseDto(req.user.id, id);
         return this.purchaseService.create(createPurchaseDto);
     }
 
     @Patch(':id')
+    @ApiBearerAuth()
+    @ApiResponse({ status: 200, description: 'Product was updated.' })
     @UsePipes(new ValidationPipe({ transform: true }))
     updateAttribute(@Body() patchProductDto: PatchProductDto, @Param('id') id: string): Promise<Product> {
         if (!validator.isMongoId(id)) throw new BadRequestException();
@@ -47,6 +60,8 @@ export class ProductsController {
     }
 
     @Delete(':id')
+    @ApiBearerAuth()
+    @ApiResponse({ status: 204, description: 'Product was deleted.'})
     @HttpCode(204)
     deleteById(@Param('id') id: string) {
         if (!validator.isMongoId(id)) throw new BadRequestException();
