3ef8b0f409eded032c07427c3c8808237b9e8ff1
Kevin <kaleman960@gmail.com>
2018-07-26 17:45:43 -0600

    Added a decorator to search in whitelist for a valid token

---
+++ src/authentication/validtoken.guard.ts
@@ -0,0 +1,17 @@
+import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import client from '../users/helpers/redis';
+
+const get = (key): Promise<boolean> => new Promise((r, rej) => client.get(key, (err, reply) => { if (err) rej(err); r(reply); }));
+
+@Injectable()
+export class TokenGuard implements CanActivate {
+  constructor(private readonly reflector: Reflector) {}
+  async canActivate(
+    context: ExecutionContext,
+  ): Promise<boolean> {
+    const request = context.switchToHttp().getRequest();
+    const { user } = request;
+    return await get(user.uuid);
+  }
+}
