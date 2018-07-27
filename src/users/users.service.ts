fe79056b619a5db80358aff9162c3cc97f767bf6
Pablo Berganza <pberganza10@gmail.com>
2018-07-26 18:32:18 -0600

    ADDED: Role and RoleMapping models

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,6 +2,8 @@ import { Model } from 'mongoose';
 import { Injectable, UnauthorizedException, NotFoundException } from '@nestjs/common';
 import { UsersDto } from './dto/users.dto';
 import { User } from './interface/users.interface';
+import { Role } from '../roles/interfaces/role.interface';
+import { RoleMapping } from '../role-mappings/interfaces/role-mapping.interface';
 import { UserInfo } from './interface/user-info.interface';
 import { InjectModel } from '@nestjs/mongoose';
 import { createJWT } from './helpers/jwt';
@@ -9,38 +11,47 @@ import client from './helpers/redis';
 
 @Injectable()
 export class UserService {
-  constructor(@InjectModel('Users') readonly userModel: Model<User>) {}
-  login(user: UsersDto): Promise<object> {
-    return this.userModel.findOne({ username: user.username }).exec().then((userFound) => {
-      if (!userFound || !userFound.comparePass(user.password)) throw new UnauthorizedException('Invalid credentials');
-      return { token: createJWT(userFound) };
-    });
-  }
-  async findById(_id: string): Promise<UserInfo> {
-    return await this.userModel.findOne({ _id }).select('username creditLimit').exec();
-  }
+    constructor(
+        @InjectModel('Users') readonly userModel: Model<User>,
+        @InjectModel('Role') private readonly roleModel: Model<Role>,
+        @InjectModel('RoleMapping') private readonly roleMappingModel: Model<RoleMapping>,
+    ) { }
 
-  signup(user: UsersDto): Promise<User> {
-    return this.userModel.create({ username: user.username, password: this.userModel.hashPassword(user.password)})
-      .then(userCreated => {
-        const userToReturn = { id: userCreated._id, username: userCreated.username, limit: userCreated.creditLimit };
-        return userToReturn;
-      });
-  }
-  async updateCreditLimit(creditLimit: number): Promise<object> {
-    const result = await this.userModel.update({}, { $set: { creditLimit }}, {multi: true}).exec();
-    if (result.ok === 1) return { message: `Updated ${result.n} users`};
-    else return null;
-  }
-  async updateOneUserCreditLimit(_id: string, creditLimit: number): Promise<object> {
-    if (!await this.userModel.findOne({ _id }).exec()) throw new NotFoundException('UserId not found');
-    const result = await this.userModel.update({ _id }, { $set: { creditLimit }}, { new: true }).exec();
-    if (result.ok === 1) return { message: `Updated credit limit for this user`};
-    else return null;
-  }
-  async logout(uuid: string): Promise<boolean> {
-    return client.del(uuid, (err, response) => {
-      return response === 1;
-    });
-  }
+    login(user: UsersDto): Promise<object> {
+        return this.userModel.findOne({ username: user.username }).exec().then(async (userFound) => {
+            if (!userFound || !userFound.comparePass(user.password)) throw new UnauthorizedException('Invalid credentials');
+            const userRoles = await this.roleMappingModel.find({ userId: user._id }).populate('roleId', '-_id name').exec();
+            const parsedRoles = userRoles.map(role => {
+                return role.roleId.name;
+            });
+            return { token: createJWT(userFound, parsedRoles) };
+        });
+    }
+    async findById(_id: string): Promise<UserInfo> {
+        return await this.userModel.findOne({ _id }).select('username creditLimit').exec();
+    }
+
+    signup(user: UsersDto): Promise<User> {
+        return this.userModel.create({ username: user.username, password: this.userModel.hashPassword(user.password) })
+            .then(userCreated => {
+                const userToReturn = { id: userCreated._id, username: userCreated.username, limit: userCreated.creditLimit };
+                return userToReturn;
+            });
+    }
+    async updateCreditLimit(creditLimit: number): Promise<object> {
+        const result = await this.userModel.update({}, { $set: { creditLimit } }, { multi: true }).exec();
+        if (result.ok === 1) return { message: `Updated ${result.n} users` };
+        else return null;
+    }
+    async updateOneUserCreditLimit(_id: string, creditLimit: number): Promise<object> {
+        if (!await this.userModel.findOne({ _id }).exec()) throw new NotFoundException('UserId not found');
+        const result = await this.userModel.update({ _id }, { $set: { creditLimit } }, { new: true }).exec();
+        if (result.ok === 1) return { message: `Updated credit limit for this user` };
+        else return null;
+    }
+    async logout(uuid: string): Promise<boolean> {
+        return client.del(uuid, (err, response) => {
+            return response === 1;
+        });
+    }
 }
