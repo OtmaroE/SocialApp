83728c32e76a8c341b1f3fe11c151282b911971b
Kevin <kaleman960@gmail.com>
2018-07-27 11:26:01 -0600

    Fixed a bug that prevented user from create an account, also, token now has roles as array

--- src/pipes/hash-password.pipe.ts
+++ src/pipes/hash-password.pipe.ts
@@ -1,15 +1,14 @@
 import { PipeTransform, Injectable, ArgumentMetadata, HttpStatus } from '@nestjs/common';
 import * as bcrypt from 'bcrypt';
-import { CreateUserDto } from '../users/dto/create-user.dto';
+import { CreateUser } from '../users/dto/create-user.dto';
 
 @Injectable()
-export class HashPasswordPipe implements PipeTransform<CreateUserDto, Promise<CreateUserDto>> {
-    async transform(user: CreateUserDto, metadata: ArgumentMetadata): Promise<CreateUserDto> {
+export class HashPasswordPipe implements PipeTransform<CreateUser, Promise<CreateUser>> {
+    async transform(user: CreateUser, metadata: ArgumentMetadata): Promise<CreateUser> {
         const hashedPassword = await bcrypt.hash(user.password, 12);
-        return new CreateUserDto(
+        return new CreateUser(
             user.username,
             hashedPassword,
-            user.email,
             user.role,
         );
     }
