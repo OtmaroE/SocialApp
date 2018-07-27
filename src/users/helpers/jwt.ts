5ed2135ef0c7bc2a4dec30e48f7c80b712220164
Pablo Berganza <pberganza10@gmail.com>
2018-07-26 18:32:08 -0600

    ADDED: roles as array

--- src/users/helpers/jwt.ts
+++ src/users/helpers/jwt.ts
@@ -5,20 +5,20 @@ import client from './redis';
 import { UnauthorizedException } from '@nestjs/common';
 const { SECRET } = process.env;
 
-export const createJWT = (user) => {
-  const tokenUUID = uuid();
-  const token = jwt.sign({ id: user._id, username: user.username, role: user.role, uuid: tokenUUID }, SECRET, { expiresIn: '12h'});
-  client.set(`${tokenUUID}`, 'token', 'EX', '43200');
-  return token;
+export const createJWT = (user, parsedRoles: string[]) => {
+    const tokenUUID = uuid();
+    const token = jwt.sign({ id: user._id, username: user.username, roles: parsedRoles, uuid: tokenUUID }, SECRET, { expiresIn: '12h' });
+    client.set(`${tokenUUID}`, 'token', 'EX', '43200');
+    return token;
 };
 
 export const validateJWT = (token) => {
-  let decoded = {};
-  try {
-    decoded = jwt.verify(token, SECRET);
-  } catch (e) {
-    if (e.name === 'JsonWebTokenError') throw new UnauthorizedException(e.message);
-    if (e.name === 'TokenExpiredError') throw new UnauthorizedException('Token has expired');
-  }
-  return decoded;
+    let decoded = {};
+    try {
+        decoded = jwt.verify(token, SECRET);
+    } catch (e) {
+        if (e.name === 'JsonWebTokenError') throw new UnauthorizedException(e.message);
+        if (e.name === 'TokenExpiredError') throw new UnauthorizedException('Token has expired');
+    }
+    return decoded;
 };
